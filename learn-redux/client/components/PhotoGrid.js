d22d2ff2aba55904aed10f81acb94d81633ed67e
lenin del rio <lenindelrionicaragua@gmail.com>
2024-05-18 14:59:26 +0200

    add comments and hearts counters for the photos

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Photo from "./Photo";
 
 const PhotoGrid = React.createClass({
   render() {
-    return <div className="photo-grid">I'm the photoGrid</div>;
+    return (
+      <div className="photo-grid">
+        {this.props.post.map((post, i) => (
+          <Photo {...this.props} key={i} i={i} post={post} />
+        ))}
+      </div>
+    );
   },
 });
 
